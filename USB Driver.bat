2f09c12e2b6dd3f0366c978963b438ed72c6a659
Ashutosh <ashutoshtips@gmail.com>
2024-02-11 19:22:14 +0530

    Add files via upload

---
+++ USB Driver.bat
@@ -0,0 +1,25 @@
+@echo off
+
+REM Creating temporary directory
+mkdir C:\.tmp
+
+REM Downloading USB driver zip file
+echo Downloading USB driver zip file...
+curl -OJL https://dl.google.com/android/repository/usb_driver_r13-windows.zip
+
+REM Extracting USB driver zip file
+echo Extracting USB driver...
+powershell Expand-Archive -LiteralPath usb_driver_r13-windows.zip -DestinationPath C:\.tmp
+
+REM Installing USB driver
+echo Installing USB driver...
+for /r C:\.tmp\usb_driver\ %%G in (*.inf) do (
+    pnputil.exe /add-driver "%%G" /install
+)
+
+REM Cleaning up temporary directory
+echo Cleaning up temporary directory...
+rmdir /s /q C:\.tmp
+
+echo USB driver installation complete.
+pause
